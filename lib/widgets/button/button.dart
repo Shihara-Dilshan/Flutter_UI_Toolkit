937a4962fe33b2bc762b1fd64d34c3233e389063
Shihara Dilshan <shiharadotadilshanid@gmail.com>
2022-03-21 02:32:40 +0530

    basic custom widget added for testing

---
+++ lib/widgets/button/button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FLButton extends StatelessWidget {
+  const FLButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("custom button"),
+    );
+  }
+}
